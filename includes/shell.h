e8536f3c4f454d3641e0e2b2eec8dda1a898e71d
Clement Sapt <csapt@student.42lyon.fr>
2021-02-04 16:02:12 +0100

    add Makefile and header :white_check_mark:

--- includes/shell.h
+++ includes/shell.h
@@ -1,36 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   shell.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csapt <csapt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/24 21:36:46 by csapt             #+#    #+#             */
-/*   Updated: 2021/02/04 15:59:29 by csapt            ###   ########lyon.fr   */
+/*   Created: 2021/02/04 15:48:34 by csapt             #+#    #+#             */
+/*   Updated: 2021/02/04 15:49:16 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
+#ifndef SHELL_H
+# define SHELL_H
 
-void example_function(void)
-{
-	return ;
-}
+#include "libft.h"
 
-int main(int ac, char **av)
-{
-	char *buf;
+void example_function(void);
 
-	(void)ac;
-	(void)av;
-	example_function();
-	buf = ft_calloc(BUFFER_SIZE, sizeof(char));
-	ft_printf("> ");
-	while (read(0, buf, BUFFER_SIZE) != 0)
-	{
-		printf("COMMAND: [%s]\n", buf);
-		ft_printf ("> ");
-	}
-	free(buf);
-	return (0);
-}
+#endif
