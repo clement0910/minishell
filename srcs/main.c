8fdcc98752a84bd47f08790a12ffd763479c0615
Clément <csapt2001@gmail.com>
2021-04-25 19:45:05 +0200

    rebuild minishell :construction:

--- srcs/main.c
+++ srcs/main.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   built_in_export.c                                  :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csapt <csapt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/09 10:31:44 by csapt             #+#    #+#             */
-/*   Updated: 2021/02/11 16:09:34 by csapt            ###   ########lyon.fr   */
+/*   Created: 2021/04/25 19:31:34 by csapt             #+#    #+#             */
+/*   Updated: 2021/04/25 19:33:47 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
+#include "libft.h"
 
-int built_in_export(char **command, char ***env_tab, t_envlst **env_list)
+int main()
 {
-	t_envlst *lst;
-	t_env *env;
-
-	if (!(env = get_env_data(command[1])))
-		return(1);
-	if (!(lst = ft_lst_new(env)))
-		return (1);
-	ft_lst_addback(env_list, lst);
-	//print_lst_tab(env_list);
-	ft_free_tab(*env_tab);
-	*env_tab = ft_lst_to_tab(env_list);
+	printf("Hello World!\n");
 	return (0);
 }
