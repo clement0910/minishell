836a99d5b95c1b1e0c720ed36cbc28d777b42ab6
Clément <csapt2001@gmail.com>
2021-04-29 17:52:40 +0200

    improved cd built_in func :zap:

--- srcs/built_in_cd.c
+++ srcs/built_in_cd.c
@@ -6,7 +6,7 @@
 /*   By: rolaforg <rolaforg@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/08 12:48:53 by rolaforg          #+#    #+#             */
-/*   Updated: 2021/04/29 14:55:48 by rolaforg         ###   ########lyon.fr   */
+/*   Updated: 2021/04/29 17:46:59 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ int	built_in_cd(char *path, char *home)
 	{
 		if (!home)
 		{
-			printf("Home is NULL");
+			ft_putendl_fd("minishell: cd: HOME not set", 1);
+			return (1);
 		}
-		if (chdir(home) == -1)
-			return(1);
+		path = home;
 	}
-	else 
+	if (chdir(path) == -1)
 	{
-		if (chdir(path) == -1)
-		{
-			return(1);
-		}
+		ft_putstr_fd("minishell: cd: ", 1);
+		ft_putstr_fd(path, 1);
+		ft_putstr_fd(": ", 1);
+		ft_putendl_fd(strerror(errno), 1);
 	}
 	return (0);
 }
--- srcs/shell_built_in.c
+++ srcs/shell_built_in.c
@@ -6,7 +6,7 @@
 /*   By: rolaforg <rolaforg@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 16:21:13 by csapt             #+#    #+#             */
-/*   Updated: 2021/04/29 15:05:51 by rolaforg         ###   ########lyon.fr   */
+/*   Updated: 2021/04/29 17:12:19 by csapt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,16 @@ int built_in_command(char *cmd, char **args, t_global *glb)
 	if (ft_strcmp("echo", cmd) == 0)
 		return (built_in_echo(args));
 	else if (ft_strcmp("cd", cmd) == 0)
-		return (built_in_cd(args[1], getenv("HOME")));
+		return (built_in_cd(args[1], get_var_name(glb->env, "HOME")));
 	else if (ft_strcmp("hello", cmd) == 0)
 		return (built_in_hello());
 	else if (ft_strcmp("pwd", cmd) == 0)
 		return (built_in_pwd());
+	else if (ft_strcmp("unset", cmd) == 0)
+		return (built_in_unset(args[1], &glb->tab_env, &glb->env));
 	else
 	{
 		glb->ret = 1;
 		return (1);
 	}
 }
-
-	// if (ft_strcmp("hello", glb->command->tab_command[0]) == 0)
-	// 	return (built_in_hello());
-	// else if (ft_strcmp("env", glb->command->tab_command[0]) == 0)
-	// 	return (built_in_env(glb->tab_env));
-	// else if (ft_strcmp("export", glb->command->tab_command[0]) == 0)
-	// 	return (built_in_export(glb->command, &glb->env, &glb->tab_env));
-	// else if (ft_strcmp("unset", glb->command->tab_command[0]) == 0)
-	// 	return (built_in_unset(glb->command, &glb->tab_env, &glb->env));
-	// else
-	// 	return (0);
